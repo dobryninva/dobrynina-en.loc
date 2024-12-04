17ffe59712b85945943ac888f50c74e56017ebca
vdrxx <vdrxx@mail.ru>
2024-12-04 14:29:02 +0300

    dfsdf

---
+++ core/cache/mgr/smarty/default/aab27e3ae9c35da1976dc3f27555c96fcf336921_0.file.index.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 3.1.48, created on 2024-12-04 13:24:08
+  from '/home/vladimir/www/html/dobrynina-en.loc/core/components/console/templates/default/index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.48',
+  'unifunc' => 'content_67502dc85133c4_11351912',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'aab27e3ae9c35da1976dc3f27555c96fcf336921' => 
+    array (
+      0 => '/home/vladimir/www/html/dobrynina-en.loc/core/components/console/templates/default/index.tpl',
+      1 => 1483943346,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_67502dc85133c4_11351912 (Smarty_Internal_Template $_smarty_tpl) {
+?><div id="mod-extra-console"></div><?php }
+}
